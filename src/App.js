5dcc60bf94db204375504b5307535ed71090f57e
Vineet Jangra <vineet-jangra>
2024-06-24 12:23:08 +0530

    changing zoom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Introduction from './Introduction/Introduction';
 import PathFindingVisualizer from './PathfindingVisualizer/PathFindingVisualizer';
 function App() {
+  document.body.style.zoom = "67%";
   return (
     <div className="App">
         <Introduction />
